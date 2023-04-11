964cc8e8aaa9dca78b0dc285f015bb24a8e759f0
Vicknesh Suppramaniam <vicknesh.suppramaniam@gmail.com>
2023-04-11 19:28:42 +0800

    function Has correct check for bit values; added custom JSON marshal and unmarshal

--- all_test.go
+++ all_test.go
@@ -1,6 +1,7 @@
 package bitmask
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -16,7 +17,7 @@ const (
 	SrvCapCrvPub
 )
 
-func TestBitmask(T *testing.T) {
+func TestBitmaskNew(T *testing.T) {
 
 	b := New(3)
 	fmt.Println("new\t", b)
@@ -25,13 +26,13 @@ func TestBitmask(T *testing.T) {
 	//b.Set(SrvCapRSA, SrvCapED)
 	fmt.Println("set\t", b)
 
-	fmt.Println("has RSA\t", b.Has(SrvCapRSA))
+	fmt.Println("has Curve\t", b.Has(SrvCapCrvPub))
 
-	b.Remove(SrvCapRSA)
+	b.Remove(SrvCapCrvPub)
 	//b.Set(SrvCapRSA, SrvCapED)
 	fmt.Println("remove\t", b)
 
-	fmt.Println("has RSA\t", b.Has(SrvCapRSA))
+	fmt.Println("has Curve\t", b.Has(SrvCapCrvPub))
 
 	b.SetAll()
 	fmt.Println("setall\t", b)
@@ -42,7 +43,7 @@ func TestBitmask(T *testing.T) {
 }
 
 func TestBitmaskString(T *testing.T) {
-	b, err := NewFromStr("100111")
+	b, err := NewFromStr("0100111")
 	if nil != err {
 		log.Println(err)
 		os.Exit(1)
@@ -50,5 +51,43 @@ func TestBitmaskString(T *testing.T) {
 	fmt.Println("new\t", b)
 
 	fmt.Println("has RSA\t", b.Has(SrvCapRSA))
+
+}
+
+func TestBitmaskJSON(T *testing.T) {
+
+	type Example struct {
+		//B *Bitmask
+		//B string `json:"B"`
+		B *Bitmask `json:"b"`
+	}
+
+	e := new(Example)
+	e.B = new(Bitmask)
+
+	e.B.SetLength(12)
+	e.B.Set(SrvCapRSA, SrvCapEC, SrvCapED, SrvCapCrvPub)
+	//fmt.Println(e.B)
+	//fmt.Println(e.b.Has(SrvCapCrvPub))
+	//e.B.Remove(SrvCapCrvPub)
+	//fmt.Println(e.b.Has(SrvCapCrvPub))
+
+	eB, err := json.Marshal(e)
+	if nil != err {
+		log.Println("marshal: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(string(eB))
+
+	ex := new(Example)
+	ex.B = new(Bitmask)
+	err = json.Unmarshal(eB, ex)
+	if nil != err {
+		log.Println("unmarshal: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("unmarshalled", ex.B)
+	fmt.Println(ex.B.Has(SrvCapCrvPub))
 
 }
--- bitop.go
+++ bitop.go
@@ -1,9 +1,11 @@
 package bitmask
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 	"strconv"
+	"strings"
 )
 
 // Bit - new type for bitmask
@@ -37,8 +39,15 @@ func NewFromStr(bitstr string) (b *Bitmask, err error) {
 		return nil, fmt.Errorf("newfromstr: error parsing bit string -> %w", err)
 	}
 
+	b.largest = 1 << b.size
 	b.bv = Bit(bitvalue)
 	return
+}
+
+// SetLength - sets the length of the bit string
+func (b *Bitmask) SetLength(len int) {
+	b.largest = 1 << len
+	b.size = len
 }
 
 // Set - sets the given bits to 1, expanding size as needed for string padding and clearing
@@ -74,6 +83,7 @@ func (b *Bitmask) SetAll() {
 // Remove - sets the given bits to 0
 func (b *Bitmask) Remove(bits ...Bit) {
 	for _, bit := range bits {
+		// //b.bv &= ^bit
 		b.bv &^= bit
 	}
 }
@@ -92,10 +102,32 @@ func (b *Bitmask) Toggle(bits ...Bit) {
 
 // Has - checks if a given bit is set
 func (b *Bitmask) Has(bit Bit) (ok bool) {
-	return (b.bv & bit) == 1
+	return (b.bv & bit) != 0
 }
 
 // String - returns a bit string with padding of size
 func (b *Bitmask) String() (bitStr string) {
 	return fmt.Sprintf("%0*b", b.size, b.bv)
+}
+
+// MarshalJSON - returnsa JSON marshal bytes for the bitmask
+func (b Bitmask) MarshalJSON() ([]byte, error) {
+	// do not change this to `[]byte(b.String())`, calling json.Marshal will return it as a proper string
+	return json.Marshal(b.String())
+}
+
+// UnmarshalJSON - returnsa unmarshaled JSON of the bitmask
+func (b *Bitmask) UnmarshalJSON(bitbytes []byte) (err error) {
+
+	bitstr := string(bitbytes)
+	bitstr = strings.ReplaceAll(bitstr, "\"", "") // strip the quotes (") before using
+
+	b1, err := NewFromStr(bitstr)
+	if nil != err {
+		return fmt.Errorf("unmarshaljson: error decoding bit string -> %w", err)
+	}
+
+	*b = *b1 // assign the temp variable to the real bitmask
+
+	return
 }
